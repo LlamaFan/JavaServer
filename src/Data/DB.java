868888965ad878512fcb19077dcc7fa3d228bd5e
llamafan <nothingemailneu@gmail.com>
2025-02-15 14:14:26 +0100

    .

--- src/Data/DB.java
+++ src/Data/DB.java
@@ -2,7 +2,11 @@ package Data;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Base64;
 
 public class DB {
     private ArrayList<User> users;
@@ -14,7 +18,7 @@ public class DB {
         users.add(new User("Person1", "password!123"));
     }
 
-    public void createMessage(int id_receiver, String content) {
+    public void createMessage(int id_sender, int id_receiver, String content, String password) {
         long[] text = new long[content.length()];
         User u = users.get(id_receiver);
 
@@ -22,21 +26,65 @@ public class DB {
             text[i] = (long) (Math.pow((int) content.charAt(i), u.getPublicKey()[0]) % u.getPublicKey()[1]);
         }
 
-        Message m = new Message(text, null); // I need to add the second text here
+        Message m = new Message(text, createSignature(id_sender, content, password), id_sender); // I need to add the second text here
         messages.add(m);
     }
 
-    public String decryptMessage(int id_reader, int id_message, String password) {
-        String decryptedText = "";
-        User u = users.get(id_reader);
-        Message m = messages.get(id_message);
-        BigInteger biN = BigInteger.valueOf(u.getPublicKey()[1]);
+    private String createSignature(int id_sender, String content, String password) {
+        content = hashText(content);
+        User u = users.get(id_sender);
+        String signature = "";
 
-        for (int i = 0; i < m.content().length; i++) {
-            BigInteger biC = BigDecimal.valueOf(m.content()[i]).toBigInteger();
-            decryptedText += (char) biC.pow(u.getPrivateKey(password)).mod(biN).intValue();
+        for (int i = 0; i < content.length(); i++) {
+            signature += String.valueOf(
+                Math.pow((int) content.charAt(i), u.getPrivateKey(password)) % u.getPublicKey()[1]);
+        }
+
+        return signature;
+    }
+
+    public String decryptMessage(int id_reader, int id_message, String password) {
+        long exponent = users.get(id_reader).getPrivateKey(password);
+        long n = users.get(id_message).getPublicKey()[1];
+        long[] deCon = messages.get(id_message).content();
+        String content = "";
+
+        for (int i = 0; i < deCon.length; i++) content += String.valueOf(deCon[i]);
+
+        String decryptedText = decryptContent(exponent, n, content);
+
+        return (decryptSignature(id_message, decryptedText)) ? decryptedText : "Message has been compromised";
+    }
+
+    private boolean decryptSignature(int message_id, String decryptedContent) {
+
+
+        return false;
+    }
+
+    private String decryptContent(long exponent, long publicN, String content) {
+        String decryptedText = "";
+        BigInteger biN = BigInteger.valueOf(publicN);
+
+        for (int i = 0; i < content.length(); i++) {
+            BigInteger biC = BigDecimal.valueOf(content.charAt(i)).toBigInteger();
+            decryptedText += (char) biC.pow((int) exponent).mod(biN).intValue();
         }
 
         return decryptedText;
+    }
+
+    private String hashText(String password) {
+        String encoded = "";
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            encoded = Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        return encoded;
     }
 }
--- src/Data/Message.java
+++ src/Data/Message.java
@@ -2,14 +2,20 @@ package Data;
 
 public class Message {
     private long[] content;
-    private long[] contentSender;
+    private String signature;
+    private int sender_id;
 
-    public Message(long[] content, long[] contentSender) {
+    public Message(long[] content, String signature, int sender_id) {
         this.content = content;
-        this.contentSender = contentSender;
+        this.signature = signature;
+        this.sender_id = sender_id;
     }
 
     public long[] content() {
         return content;
+    }
+
+    public int getSender_id() {
+        return sender_id;
     }
 }
